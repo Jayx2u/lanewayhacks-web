b8d3e9c59d0a7610db2ab7a5ba6ce92e0fefb9bc
Jay <141212189+jayx2u@users.noreply.github.com>
2025-02-17 21:02:42 +1100

    Add logo to PageTransition component and change timings

--- src/components/animations/page-transition.tsx
+++ src/components/animations/page-transition.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import Image from 'next/image'
 
 interface PageTransitionProps {
   children: React.ReactNode;
@@ -11,23 +12,40 @@ const PageTransition = ({ children }: PageTransitionProps) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { duration: 0.5, ease: "easeInOut" },
-      }}
-      onAnimationComplete={() => document.body.style.overflow = "auto"}
-      className="will-change-opacity"
-    >
-      {!isLoading && children}
-    </motion.div>
+     <div className="relative">
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut", delay: 1.5 }}
+        className="fixed inset-0 flex items-center justify-center bg-[#281c30] z-50"
+      >
+        <Image
+          src="/laneway-logo.png"
+          alt="LanewayHacks"
+          width={400}
+          height={400}
+          priority
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { duration: 0.5, ease: "easeInOut", delay: 1.8 },
+        }}
+        onAnimationComplete={() => document.body.style.overflow = "auto"}
+        className="will-change-opacity"
+      >
+        {!isLoading && children}
+      </motion.div>
+    </div>
   );
 };
 
