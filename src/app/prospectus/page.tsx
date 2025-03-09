91cc8a2e434d2c6ac0a7bdb2a34e8f1d97aba203
Brandon Zhou <s1072489@haileybury.com.au>
2025-03-09 21:29:14 +1100

    Simplify things...

--- src/app/prospectus/page.tsx
+++ src/app/prospectus/page.tsx
@@ -1,19 +1,13 @@
 'use client'
 
-import { useParams } from 'next/navigation'
-import React from "react"
+import React from 'react'
+
 
 export default function DownloadPage() {
-    const { filename } = useParams()
-
     React.useEffect(() => {
-        if (filename=="prospectus") {
             window.location.href = `https://raw.githubusercontent.com/Jayx2u/lanewayhacks-web/refs/heads/main/public/files/Laneway_Hacks_Prospectus_2025.pdf`
-        }
-        else {
-            window.location.href = `https://raw.githubusercontent.com/Jayx2u/lanewayhacks-web/refs/heads/main/public/files/${filename}`
-        }
-    }, [filename])
+        
+    })
 
     return (
         <div className="flex flex-col space-y-8 mx-4 justify-center items-center min-h-screen text-hack-white">
